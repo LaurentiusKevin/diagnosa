947fba26f9abe08abf9927314b44f8720d2b7467
Laurentius Kevin <laurentiuskevin44@gmail.com>
2019-05-13 00:08:52 +0700

    menambah list penyakit dan gejala

--- daftarPenyakit/views.py
+++ daftarPenyakit/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import DataAsli
 
 # Create your views here.
 def daftarPenyakitView(request):
-    return render(request, 'daftar-penyakit.html')
+    all_data_penyakit = DataAsli.objects.all()
+    return render(request, 'daftar-penyakit.html',
+        {'all_data': all_data_penyakit})
